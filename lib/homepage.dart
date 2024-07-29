449ffd2ae1e69d453b6e0e396aad6200e646e5f8
Heng To <heng.to@rupp.edu.kh>
2024-07-29 18:02:14 +0700

    initialize

---
+++ lib/homepage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+        
+        title: Text('Shop'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {
+              // Add your notifications action here
+            },
+          ),
+        ],
+        backgroundColor: Colors.grey.shade300,
+    )
+    );
+  }
+}
---
+++ lib/theme/light_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightmode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Colors.grey.shade300,
+    primary: Colors.red.shade800,
+    secondary: Colors.white,
+    inversePrimary: Colors.grey.shade700
+  )
+);
